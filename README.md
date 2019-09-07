3da50406ce6a66cd565ce7a04248fae8a1eca001
Veeraswamy Gatta <veegatta@in.ibm.com>
2019-09-07 22:50:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot on Docker connecting to Kafka Docker container
+
+1.Run the docker-compose (docker-compose up)
---
+++ src/main/java/com/ibm/kafka/KafkaintroApplication.java
@@ -0,0 +1,12 @@
+package com.ibm.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaintroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaintroApplication.class, args);
+    }
+}
